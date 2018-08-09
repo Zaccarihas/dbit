2d5642cbe5106a45b05d32dbbe5c5e271e9439f8
Zaccarihas <lofqvist.anders@gmail.com>
2018-08-09 13:45:40 +0200

    Created a recursive aggregated Total function

--- lst_Account_Status.php
+++ lst_Account_Status.php
@@ -1,8 +1,40 @@
 <?php
    
 require_once 'resources/fnc_database.php';
+require_once 'resources/fnc_general.php';
 
-function getAccounts($q_subAccs,$q_latestStat,$q_transTot,$parent = null){
+function recAgrTotal ($q_subAccs,$q_latestStat,$q_transTot,$acc,$start,$end){
+    
+    $q_transTot->execute(array($start,$end,$acc));
+    $agrTot = $q_transTot->fetch();
+    
+    $q_subAccs->execute(array($acc));
+    $subAccs = $q_subAccs->fetchAll();
+    
+    foreach($subAccs as $sub){
+        $agrTot += recAgrTotal($q_subAccs,$q_latestStat,$q_transTot,$acc,$start,$end);
+    }
+    
+    return $agrTot;
+}
+
+function agrTotal ($q_subAccs,$q_latestStat,$q_transTot,$acc = null,$startDate = null,$endDate=null){
+    
+    // If no startDate has been provided, set the startDate to the date and time of the latest statement
+    if($startDate=null){
+        $q_latestStat->execute(array($acc));
+        $datelimit = $q_latestStat->fetch();
+    }
+    
+    // If no endDate has been provided, set the endDate to the date and time to the current date and time
+    if($endDate=null){
+        $endDate = timestamp();
+    }
+    
+    return  recAgrTotal ($q_subAccs,$q_latestStat,$q_transTot,$acc,$startDate,$endDate);
+}
+
+/*function getAccounts($q_subAccs,$q_latestStat,$q_transTot,$parent = null){
     
     $q_subAccs->execute(array($parent));
     $subAccs = $q_subAccs->fetchAll();
@@ -26,46 +58,22 @@ function getAccounts($q_subAccs,$q_latestStat,$q_transTot,$parent = null){
     }
     
     return $agrTot;
-}
+}*/
    
    $db = databaseconnect('lofqvist.dynu.net','dev','Av4rak1n','dbit');
-   
-   echo "<h1>Welcome</h1>";
-   echo "<div>The database is now open for business.</div><div>This is the requested information:</div>";
-   
-   
-   
-   /* Alternativ ett - Att hämta ut så mycket som möjligt med en stor SQL-fråga.
-    * 
-    *    Detta verkar dock inte som en bra ide då frågan blir svår att tyda och min nuvarande
-    *    databas inte stöd för rekursiva frågor (mariadb 10.1)    * 
-    * 
-   
-   $qry  = "SELECT tbl1.AccBDC, tbl1.AccName,tbl1.AccStated, tbl1.AccStat, tbl2.Trans, tbl1.AccStat + tbl2.Trans AS Total ";
-   $qry .= "FROM (SELECT acc.AccountID AS Account, acc.AccBDC, acc.AccName, sts.AccStated, sts.AccStat FROM tbl_accounts AS acc ";
-   $qry .= "LEFT JOIN (SELECT Account, MAX(Date) AS AccStated, Amount AS AccStat FROM tbl_statements GROUP BY Account) AS sts ";
-   $qry .= "ON acc.AccountID = sts.Account) AS tbl1 ";
-   $qry .= "LEFT JOIN (SELECT trn.Account,SUM(trn.Amount) AS Trans ";
-   $qry .= "FROM tbl_transactions AS trn LEFT JOIN tbl_occasions AS occ ON trn.Occasion = occ.ID ";
-   $qry .= "WHERE occ.Date > (SELECT AccStated FROM (SELECT Account, MAX(Date) AS AccStated FROM tbl_statements GROUP BY Account) AS sts ";
-   $qry .= "WHERE trn.Account = sts.Account) GROUP BY trn.Account) AS tbl2 ON tbl1.Account = tbl2.Account";    */
-   
-   
-   /* Alternativ två - Hämta ut alla toppkonton och iterera över dessa.
-    * 
-    *    Inom iterationen så beräknas aggregerat resultat från under konton
-    * 
-    */
    
    // Prepare needed qureies 
    
    $qry_subAccs =   $db->prepare("SELECT AccountID, AccBDC, AccName FROM tbl_accounts WHERE Parent = ?");
    $qry_latestStat= $db->prepare("SELECT MAX(Date) AS AccStated, Amount AS AccStat FROM tbl_statements WHERE Account = ?");
-   $qry_transTot =  $db->prepare("SELECT SUM(trn.Amount) AS Trans FROM tbl_transactions AS trn LEFT JOIN tbl_occasions AS occ ON trn.Occasion = occ.ID WHERE occ.Date > ? AND trn.Account = ?";
+   $qry_transTot =  $db->prepare("SELECT SUM(trn.Amount) AS Trans FROM tbl_transactions AS trn LEFT JOIN tbl_occasions AS occ ON trn.Occasion = occ.ID WHERE occ.Date > ? AND occ.Date <= ? AND trn.Account = ?");
+   
+   echo "<h1>Welcome</h1>";
+   echo "<div>The database is now open for business.</div><div>This is the requested information:</div>";
    
    echo "<TABLE><TBODY>"; 
    
-   presentAcc($qry_subAccs,$qry_latestStat,$qry_transTot);
+   //presentAcc($qry_subAccs,$qry_latestStat,$qry_transTot);
 
    
    echo "</TBODY>";
