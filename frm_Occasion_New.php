62d692c3c866019485552e45aa24053501d25ee1
Zaccarihas <lofqvist.anders@gmail.com>
2018-10-16 20:41:07 +0200

    Enkelt inmatningsformulär för Occasions
    Skapat ett enkelt inmatningsformulär som är byggt för att  nyttja temporära tabeller i databasen för att lagra inmatade värden innan datat överförs till de permanenta tabellerna

--- frm_Occasion_New.php
+++ frm_Occasion_New.php
@@ -1,3 +1,60 @@
+<?php 
+
+require_once 'resources/fnc_database.php';
+
+$db = databaseconnect("lofqvist.dynu.net","dev","Av4rak1n","dbit");
+
+print_r($_POST);
+
+// Handle submitted form
+
+if(isset($_POST['btnSubmit'])){
+    
+
+    // Create temporary table
+
+    $qrystr  = "CREATE TABLE tmp_Occ (";
+    $qrystr .= "OccDesc VARCHAR(64),";
+    $qrystr .= "OccDate DATETIME,";                            // Skulle kunna lägga till DEFAULT för att sätta aktuell tid direkt i db
+    $qrystr .= "OccCPart BIGINT(20) UNSIGNED,";
+    $qrystr .= "OccNote VARCHAR(64)";
+    $qrystr .= ")";
+
+    $qry = $db->prepare($qrystr);
+    $qry->execute();
+
+    // Put data into the temporary table
+
+    $qrystr  = "INSERT INTO tmp_Occ (OccDesc,OccDate,OccCPart,OccNote) ";
+    $qrystr .= "VALUES(?,?,?,?)";
+    
+    $qry = $db->prepare($qrystr);
+    $qry->execute(array($_POST['fldDesc'],$_POST['fldDate'],$_POST['fldCPart'],$_POST['fldNote']));
+    
+    // Transfer data from temporary table to permanent table
+    
+    $qrystr  = "INSERT INTO fixed_occ(OccDesc, OccDate, OccCPart, OccNote) ";
+    $qrystr .= "SELECT OccDesc,OccDate,OccCPart,OccNote FROM tmp_occ";
+    
+    $qry = $db->prepare($qrystr);
+    $qry->execute();
+    
+
+    // Drop temporary table.
+    
+    $qrystr  = "DROP TABLE tmp_occ";
+    
+    $qry = $db->prepare($qrystr);
+    $qry->execute();
+
+    
+}
+
+$db = null;
+
+
+?>
+
 <!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
@@ -6,22 +63,22 @@
 	</head>
 	<BODY>
 
-		<FORM>
+		<FORM Method="post">
 		
 			<DIV>New</DIV>
-			<DIV><DIV>Date</DIV><DIV><INPUT Name="fldDate" Size="10" Type="text"/></DIV></DIV>
-			<DIV><DIV>Description</DIV><DIV><INPUT Name="fldDesc" Size="80" Type="text"/></DIV></DIV>
+			<DIV><DIV>Date</DIV><DIV><INPUT Id="fldDate" Name="fldDate" Size="10" Type="text"/></DIV></DIV>
+			<DIV><DIV>Description</DIV><DIV><INPUT Id="fldDesc" Name="fldDesc" Size="80" Type="text"/></DIV></DIV>
 			<DIV>
 				<DIV>Counterpart</DIV>
 				<DIV>
-					<SELECT id="fldCPart" Size="5">
-						<OPTION id="fldPartOpt_1" Value="61">Coop Konsum, Hörnett</OPTION>
-						<OPTION id="fldPartOpt_2" Value="189">OKQ8 Rondellen, Örnsköldsvik</OPTION>
+					<SELECT Id="fldCPart" Name="fldCPart" Size="5">
+						<OPTION Id="fldPartOpt_1" Value="61">Coop Konsum, Hörnett</OPTION>
+						<OPTION Id="fldPartOpt_2" Value="189">OKQ8 Rondellen, Örnsköldsvik</OPTION>
 					</SELECT>				
 				</DIV>
 			</DIV>
-			<DIV><DIV>Note</DIV><DIV><TEXTAREA Name="fldNote" rows="5" cols="80" style="resize: none"></TEXTAREA></DIV></DIV>
-			<DIV><button>Submit</button></DIV>
+			<DIV><DIV>Note</DIV><DIV><TEXTAREA Id="fldNote" Name="fldNote" rows="5" cols="80" style="resize: none"></TEXTAREA></DIV></DIV>
+			<DIV><button Name="btnSubmit" Id="btnSubmit">Submit</button></DIV>
 		</FORM>
 
     </BODY>
